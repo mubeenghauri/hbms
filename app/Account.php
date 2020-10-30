44bfc20c965fc47fdf9f5c581bc69e73e791fb0d
mubeenghauri <p176107@nu.edu.pk>
2020-10-30 18:20:12 +0500

    Added Account model

---
+++ app/Account.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'account_id';
+
+    protected $fillable = ['name', 'owner', 'description', 'created_on'];
+
+    /**
+     * Verifies if an account exits in database
+     * 
+     * @param $account : string
+     * @return true | false
+     */
+    static function accountExists($account) {
+    	$acc = Account::where("name", $account)->get();
+        $status = false;
+    	sizeof($acc) == 0 ? $status =  false : $status =  true;
+        return $status;
+    }
+
+}
